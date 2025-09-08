1d348ddbe19c37fd39c542f6b27a844c9758518c
Tanishq Shukla 725 <fayoga3821@gmail.com>
2025-09-08 11:15:01 +0530

    Time: 1 ms (100.00%) | Memory: 45.2 MB (43.94%) - LeetSync

--- 4008-restore-finishing-order/restore-finishing-order.java
+++ 4008-restore-finishing-order/restore-finishing-order.java
@@ -1,12 +1,24 @@
 class Solution {
     public int[] recoverOrder(int[] order, int[] friends) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < order.length; i++) map.put(order[i], i);
-        PriorityQueue<Integer> pq = new PriorityQueue<>((x, y) -> (map.get(x) - map.get(y)));
-        for (int friend: friends) pq.offer(friend);
-        int[] res = new int[pq.size()];
-        int index = 0;
-        while (!pq.isEmpty()) res[index++] = pq.poll();
-        return res;
+        
+        int n = order.length;
+        int m = friends.length;
+
+        int idx = 0;
+        int[] ans = new int[m];
+        
+
+        for (int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+
+                if (friends[j] == order[i]) {
+                    ans[idx] = order[i];
+                    
+                    idx++;
+                }
+            }
+        }
+
+        return ans;
     }
 }
