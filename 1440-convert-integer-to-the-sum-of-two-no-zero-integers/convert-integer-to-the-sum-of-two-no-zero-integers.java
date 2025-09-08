e5da3556be30f0efe5b5318d1b85584a3371a667
Tanishq Shukla 725 <fayoga3821@gmail.com>
2025-09-08 12:55:20 +0530

    Time: 1 ms (36.38%) | Memory: 41.2 MB (77.34%) - LeetSync

--- 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.java
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.java
@@ -1,28 +1,40 @@
 class Solution {
+
     public int[] getNoZeroIntegers(int n) {
-        int[] res = {0};
-        for (int i = 1; i < n; i++) {
-            int a = i;
-            if (!containsZero(a)) {
-                int b = n - a;
-                if (!containsZero(b)) {
-                    res = new int[]{a, b};
-                    break;
-                }
+        for (int A = 1; A < n; ++A) {
+            int B = n - A;
+            if (
+                !String.valueOf(A).contains("0") &&
+                !String.valueOf(B).contains("0")
+            ) {
+                return new int[] { A, B };
             }
         }
-        return res;
+        return new int[0];
     }
-
-    boolean containsZero(int num) {
-    while (num > 0) {
-        int digit = num % 10;   
-        if (digit == 0) {
-            return true;
-        }
-        num = num / 10;         
-    }
-    return false;
 }
+//         int[] res = {0};
+//         for (int i = 1; i < n; i++) {
+//             int a = i;
+//             if (!containsZero(a)) {
+//                 int b = n - a;
+//                 if (!containsZero(b)) {
+//                     res = new int[]{a, b};
+//                     break;
+//                 }
+//             }
+//         }
+//         return res;
+//     }
 
-}
+//     boolean containsZero(int num) {
+//     while (num > 0) {
+//         int digit = num % 10;   
+//         if (digit == 0) {
+//             return true;
+//         }
+//         num = num / 10;         
+//     }
+//     return false;
+// }
+
