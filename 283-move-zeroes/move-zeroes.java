4a9cb594b986bc6d71d9a9c4e4cdc0b7be2f1508
Tanishq Shukla 725 <fayoga3821@gmail.com>
2025-09-21 08:30:22 +0530

    Time: 0 ms (100.00%) | Memory: 46.1 MB (69.22%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,16 +1,19 @@
 class Solution {
-    public void moveZeroes(int[] nums) {
+    static{
+        for(int i = 0; i< 500; i++){
+            moveZeroes(new int[]{});
+        }
+    }
+    public static void moveZeroes(int[] nums) {
+        int j = 0;
         
-        int n = nums.length;
-
-        for(int i=0; i<n; i++){
-            for(int j=1; j<n; j++){
-                if(nums[j-1] == 0){
-                    nums[j-1] = nums[j];
-                    nums[j] =0;
-                }
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != 0){
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                j++;
             }
-            
         }
     }
 }
