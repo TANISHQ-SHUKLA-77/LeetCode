7317023491ba15288eb0882e1e27cc87fba64f9b
Tanishq Shukla 725 <fayoga3821@gmail.com>
2025-09-26 19:38:25 +0530

    Time: 12 ms (100.00%) | Memory: 44 MB (5.68%) - LeetSync

--- 611-valid-triangle-number/valid-triangle-number.java
+++ 611-valid-triangle-number/valid-triangle-number.java
@@ -1,16 +1,43 @@
 class Solution {
     public int triangleNumber(int[] nums) {
-        int count=0;
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                for(int k=j+1;k<nums.length;k++){
-                    if(nums[i]+nums[j]>nums[k]){
-                        count++;
-                    }
+        int MAX = Arrays.stream(nums).max().getAsInt();
+        int oc[] = new int[MAX+1], sum[] = new int[MAX+1];
+        for(var x : nums){
+            if(x > 0)
+            oc[x]++;
+        }
+        for(int i = 0; i<=MAX; i++){
+            sum[i] = oc[i];
+            if(i>0)
+            sum[i] += sum[i-1];
+        }
+        print(oc);
+        print(sum);
+        int ans = 0;
+        for(int i = 1; i<=MAX; i++){
+            if(oc[i] > 1){
+
+                ans += (sum[Math.min(MAX, i+i-1)] - sum[i]) * oc[i] * (oc[i] - 1) / 2;
+                if(oc[i] > 2){
+
+                    ans += oc[i] * (oc[i] - 1) * (oc[i] - 2) / 6;
+                }
+            }
+            for(int j = i+1; j<=MAX; j++){
+                if(oc[i] * oc[j] == 0)continue;
+
+                int third = sum[Math.min(MAX, i+j-1)] - sum[j];
+                ans += oc[i] * oc[j] * third;
+                if(oc[j] > 1){
+
+                    ans += oc[i] * oc[j] * (oc[j] - 1) / 2;
                 }
             }
         }
-        return count;
+        return ans;
     }
+    private static void print(Object ...args){
+        // System.out.println(Arrays.deepToString(args));
+    }
+
 }
