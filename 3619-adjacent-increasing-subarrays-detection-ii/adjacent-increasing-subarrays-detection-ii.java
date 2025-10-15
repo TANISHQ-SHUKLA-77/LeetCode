fc306b6858b21f9365c76e5ec19088e1659bd3bd
Tanishq Shukla 725 <fayoga3821@gmail.com>
2025-10-15 06:45:41 +0530

    Time: 7 ms (100.00%) | Memory: 91.9 MB (7.45%) - LeetSync

--- 3619-adjacent-increasing-subarrays-detection-ii/adjacent-increasing-subarrays-detection-ii.java
+++ 3619-adjacent-increasing-subarrays-detection-ii/adjacent-increasing-subarrays-detection-ii.java
@@ -1,7 +1,9 @@
 class Solution {
     public int maxIncreasingSubarrays(List<Integer> nums) {
        
-        int n = nums.size();
+       Integer[] arr = nums.toArray(Integer[]::new);
+
+        int n = arr.length;
         int i = 0;
         int ans = 0;
         int prev = 0;
@@ -9,10 +11,8 @@ class Solution {
         while (i < n) {
             int start = i;
 
-            while (i + 1 < n && nums.get(i) < nums.get(i + 1)) {
-                i++;
-            }
-
+            while (i + 1 < n && arr[i] < arr[i+1]) i++;
+            
             int curr = i - start + 1;
             ans = Math.max(ans, Math.max(curr >> 1, Math.min(curr, prev)));
             
