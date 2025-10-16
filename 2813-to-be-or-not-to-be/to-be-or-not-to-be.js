3261dcac62b0bf254ed40c91221c1c15720cfa36
Tanishq Shukla 725 <fayoga3821@gmail.com>
2025-10-16 08:27:48 +0530

    Time: 0 ms (100.00%) | Memory: 54.5 MB (14.00%) - LeetSync

---
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+
+const fs = require('fs');
+
+var expect = function(val) {
+    return {
+        toBe:function(otherVal) {
+            if(val===otherVal) return true;
+            else throw new Error("Not Equal");
+        },
+        notToBe: function(otherVal){
+            if(val!==otherVal) return true;
+            else throw new Error("Equal");
+        }
+    }
+};
+
+process.on('exit', () => {
+    fs.writeFileSync("display_runtime.txt", "0");
+});
+
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
