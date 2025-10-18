d8694e0998c0a2db9f3e40372fa3377c946a33c3
Tanishq Shukla 725 <fayoga3821@gmail.com>
2025-10-18 13:28:37 +0530

    Time: 54 ms (10.53%) | Memory: 53 MB (92.28%) - LeetSync

---
+++ 2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+
+//  const fs = require('fs');
+ 
+var map = function(arr, fn) {
+    
+    let ans = [];
+
+    for(let i=0; i< arr?.length; i++) ans[i] = fn(arr[i], i);
+
+    return ans;
+
+}
+
+// process.on('exit', () => {
+//     fs.writeFileSync("display_runtime.txt", "0");
+// });
