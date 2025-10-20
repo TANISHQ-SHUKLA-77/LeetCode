d1776209b4deda3097005bb03cf1212d9ac6108a
Tanishq Shukla 725 <fayoga3821@gmail.com>
2025-10-20 23:08:42 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (77.82%) - LeetSync

--- 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.java
+++ 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.java
@@ -1,15 +1,17 @@
 class Solution {
+    private int n;
     public int finalValueAfterOperations(String[] operations) {
-        int ans = 0;
-        for(String str : operations){
-            if(str.charAt(0) == '+' || str.charAt(2) == '+'){
-                ans++;
-            }
-            else if(str.charAt(0) == '-' || str.charAt(2) == '-'){
-                ans--;
+        n = operations.length;
+        int negatives = 0;
+        int positives = 0;
+        for(int i = 0; i < n; i++) {
+            if(operations[i].charAt(0) == '+' || operations[i].charAt(operations[i].length() - 1) == '+') {
+                positives++;
+            } else {
+                negatives++;
             }
         }
 
-        return ans;
+        return positives - negatives;
     }
 }
