73e2e61abc016ecb8b7fa330aa16ed52327764fd
Tanishq Shukla 725 <fayoga3821@gmail.com>
2025-11-01 18:21:02 +0530

    Time: 53 ms (93.59%) | Memory: 54.4 MB (90.21%) - LeetSync

--- 2807-execute-asynchronous-functions-in-parallel/execute-asynchronous-functions-in-parallel.js
+++ 2807-execute-asynchronous-functions-in-parallel/execute-asynchronous-functions-in-parallel.js
@@ -1,23 +1,36 @@
 /**
  * @param {Array<Function>} functions
  * @return {Promise<any>}
+ 1 массив резалт, счетчик для промисов, перебор функций, внутри запучкаем, , если ок то результат записываем в резалт счетчик палсс пласс проверяем если счетчик длинне массива то резолв. кетч еррор с реджет и если массив 0 то резолвим
  */
 var promiseAll = function(functions) {
-    
-    return new Promise (( resolve, reject) =>{
 
-        let res = Array(functions.length)
-        let waitingFor= functions.length
+    return new Promise ((resolve, reject) => {
 
-        for( let i =0; i< functions.length; i++){
-             functions[i]().then((result)=>{
-                res[i] = result
-                waitingFor--
-                if(waitingFor===0) resolve(res)
-             }
-             ).catch(reject)
+const result = [];
+let counter = 0;
+
+functions.forEach((fn, index) => {
+    fn()
+    .then(res => {
+        result[index] = res;
+        counter++;
+        
+        if(counter === functions.length){
+            resolve(result)
         }
     })
+    .catch(err => reject(err))
+});
+
+if(functions.length === 0) {
+    resolve([])
+}
+
+
+
+    })
+    
 };
 
 /**
