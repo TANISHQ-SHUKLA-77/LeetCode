3b4b13859f491f49c85bed76a446ea7d8d592f70
Tanishq Shukla 725 <fayoga3821@gmail.com>
2025-11-02 17:06:26 +0530

    Time: 17 ms (100.00%) | Memory: 147 MB (5.11%) - LeetSync

--- 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.java
+++ 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.java
@@ -1,49 +1,89 @@
 class Solution {
-    private int MAX=Integer.MAX_VALUE;
-    private int MIN= Integer.MIN_VALUE;
     public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
-        int[][] grid = new int[m][n];
-        for (int[] wall : walls) {
-            grid[wall[0]][wall[1]] = MIN;
-        }
-
-        for(int[] guard:guards){
-            grid[guard[0]][guard[1]]=MAX;
-        }
-
-        for(int i=0;i<m;i++)
+          int visit[][]=new int[m][n];
+          int count=0;
+          int gr=guards.length;
+          int wr=walls.length;
+          for(int[] wall:walls)
+          {
+            visit[wall[0]][wall[1]]=2;
+          }
+          for(int gu[]:guards)
         {
-            for(int j=0;j<n;j++)
-            {
-                if(grid[i][j]==MAX)
+            visit[gu[0]][gu[1]]=2;
+        }
+          int[][] dxdy={{-1,0},{1,0},{0,-1},{0,1}};
+          for(int row=0;row<gr;row++){
+                int dx=guards[row][0];
+                int dy=guards[row][1];
+                for(int i=dx+1;i<m;i++)
                 {
-                    for(int k=j+1;k<n;k++){
-                        if(grid[i][k]==MAX || grid[i][k]==MIN) break;
-                        grid[i][k]-=1;
+                    int newdx=i;
+                    int newdy=dy;
+                    if(visit[newdx][newdy]==2)
+                    {
+                        break;
+                    } 
+                    if(visit[newdx][newdy]==1)
+                    {
+                        continue;
                     }
-                    for(int k=j-1;k>=0;k--){
-                        if(grid[i][k]==MAX || grid[i][k]==MIN) break;
-                        grid[i][k]-=1;
-                    }
-                    for(int k=i+1;k<m;k++){
-                        if(grid[k][j]==MAX || grid[k][j]==MIN) break;
-                        grid[k][j]-=1;
-                    }
-                    for(int k=i-1;k>=0;k--){
-                        if(grid[k][j]==MAX || grid[k][j]==MIN) break;
-                        grid[k][j]-=1;
-                    }
+                    visit[newdx][newdy]=1;
+                    count++;
                 }
-            }
-        }
-        int count=0;
-        
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==0) count++;
-            }
-        }
+                for(int i=dy+1;i<n;i++)
+                {
+                    int newdx=dx;
+                    int newdy=i;
+                    if(visit[newdx][newdy]==2)
+                    {
+                        break;
+                    }
+                    if(visit[newdx][newdy]==1){
+                        continue;
+                    }
+                    visit[newdx][newdy]=1;
+                    count++;
 
-        return count;
+                }
+                for(int i=dy-1;i>=0;i--)
+                {
+                    int newdx=dx;
+                    int newdy=i;
+                    if(visit[newdx][newdy]==2)
+                    {
+                        break;
+                    }
+                        
+                    if(visit[newdx][newdy]==1){
+                        continue;
+                    }
+                    visit[newdx][newdy]=1;
+                    count++;
+
+                }
+                for(int i=dx-1;i>=0;i--)
+                {
+                    int newdx=i;
+                    int newdy=dy;
+                    if(visit[newdx][newdy]==2)
+                    {
+                        break;
+                    }
+                        
+                    if(visit[newdx][newdy]==1){
+                        continue;
+                    }
+                    visit[newdx][newdy]=1;
+                    count++;
+                }
+            
+          }
+          
+          int sum=(m*n)-(gr+wr+count);
+          return sum;
+
     }
+   
+
 }
