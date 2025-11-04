de2a38bbf76028afd3f946272a0c545db84bde45
Tanishq Shukla 725 <fayoga3821@gmail.com>
2025-11-04 18:32:54 +0530

    Time: 49 ms (31.75%) | Memory: 54.3 MB (24.26%) - LeetSync

--- 2734-array-prototype-last/array-prototype-last.js
+++ 2734-array-prototype-last/array-prototype-last.js
@@ -3,8 +3,9 @@
  */
 Array.prototype.last = function() {
     
-    return this.length === 0 ? -1 : this[this.length - 1];
-
+    if (this.length === 0) return -1;
+    
+    else return this[this.length - 1];
 };
 
 /**
