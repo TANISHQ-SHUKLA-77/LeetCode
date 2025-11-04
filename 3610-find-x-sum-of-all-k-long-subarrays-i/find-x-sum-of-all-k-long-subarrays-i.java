1da3f41da4d2a510908f0cfcbdeb35466e5df5b1
Tanishq Shukla 725 <fayoga3821@gmail.com>
2025-11-04 18:30:53 +0530

    Time: 3 ms (100.00%) | Memory: 46.6 MB (7.59%) - LeetSync

--- 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.java
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.java
@@ -1,29 +1,49 @@
 class Solution {
     public int[] findXSum(int[] nums, int k, int x) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int [] res = new int [nums.length - k + 1];
+        int[] result = new int[nums.length - k + 1];
 
-        for (int i = 0, j = 0, r = 0; j < nums.length; j++) {
-            map.put(nums[j], map.getOrDefault(nums[j], 0) + 1);
-
-            if (j >= k - 1) {
-                Queue<int []> q = new PriorityQueue<>((a,b) -> b[1] == a[1] ? b[0] - a[0] : b[1] - a[1]);
-                for (int key : map.keySet()) {
-                    q.add(new int [] {key, map.get(key)});
-                }
-
-                int m = x, sum = 0;
-
-                while (!q.isEmpty() && m-- > 0) {
-                    int [] a = q.remove();
-                    sum += a[0] * a[1];
-                }
-
-                res[r++] = sum;
-                map.put(nums[i], map.get(nums[i++]) - 1);
-            }
+        for (int i = 0; i < result.length; i++) {
+            int left = i, right = i + k - 1;
+            result[i] = findXSumofSubArray(nums, left, right, x);
         }
 
-        return res;
+        return result;
+    }
+
+    private int findXSumofSubArray(int[] nums, int left, int right, int x) {
+        int sum = 0, distinctCount = 0;
+        int[] freq = new int[51]; 
+
+        for (int i = left; i <= right; i++) {
+            sum += nums[i];
+            if (freq[nums[i]] == 0) {
+                distinctCount++;
+            }
+            freq[nums[i]]++;
+        }
+
+        if (distinctCount < x) {
+            return sum;
+        }
+
+        sum = 0;
+        for (int pick = 0; pick < x; pick++) {
+            int bestFreq = -1;
+            int bestVal = -1;
+
+            for (int val = 50; val >= 1; val--) {
+                if (freq[val] > bestFreq) {
+                    bestFreq = freq[val];
+                    bestVal = val;
+                }
+            }
+
+            if (bestVal != -1) {
+                sum += bestVal * bestFreq;
+                freq[bestVal] = 0;
+            }
+        }
+        
+        return sum;
     }
 }
