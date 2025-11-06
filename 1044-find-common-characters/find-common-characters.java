ddb5c80330a8b4667e768cc2a1c2d18b9d198ae6
Tanishq Shukla 725 <fayoga3821@gmail.com>
2025-11-06 11:46:33 +0530

    Time: 3 ms (89.16%) | Memory: 44.7 MB (23.13%) - LeetSync

--- 1044-find-common-characters/find-common-characters.java
+++ 1044-find-common-characters/find-common-characters.java
@@ -1,35 +1,28 @@
 class Solution {
     public List<String> commonChars(String[] words) {
-        int[] currarr = new int[26];
-        int[] comparearr = new int[26];
-        List<String> list = new ArrayList<>();
-
-        for(char ch : words[0].toCharArray()){
-            currarr[ch - 97] = currarr[ch - 97] + 1;
+        List<String> list=new ArrayList<>();
+        String first=words[0];
+        int[] common=new int[26];
+        for(char c: first.toCharArray()) {
+            common[c-'a']++;
         }
-        if(words.length > 1){
-            for(int i = 1 ; i < words.length ; i++){
-                for(char ch : words[i].toCharArray()){
-                    comparearr[ch - 97] = comparearr[ch - 97] + 1;
-                }
-                for(int j = 0 ; j < 26 ; j++){
-                    currarr[j] = Math.min(currarr[j],comparearr[j]);
-                    comparearr[j] = 0;
-                }
+        for(int i=1;i<words.length;i++) {
+            String a=words[i];
+            int[] current=new int[26];
+            for(char c: a.toCharArray()) {
+                current[c-'a']++;
             }
-            for(int i = 0 ; i < 26 ; i++){
-            if(currarr[i]>0){
-                char ch = (char)(i+97);
-                for(int j = 0 ; j <currarr[i];j++){
-                    list.add(String.valueOf(ch));
+            for(int j=0;j<26;j++) {
+                if(common[j]>current[j]) {
+                    common[j]=current[j];
                 }
             }
         }
-        }else{
-            for (char ch : words[0].toCharArray()) {
-            list.add(String.valueOf(ch));
-        }
-   
+        for(int k=0;k<26;k++) {
+            while(common[k]>0) {
+                list.add(String.valueOf((char)(k+'a')));
+                common[k]--;
+            }
         }
         return list;
     }
