2f8745babd6c4122f99c72be847f3b2a9a99dbff
Tanishq Shukla 725 <fayoga3821@gmail.com>
2025-11-06 11:54:04 +0530

    Time: 24 ms (98.41%) | Memory: 59.2 MB (5.07%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,35 +1,35 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        if (nums.length < 3) return res;
-
+        int n = nums.length;
         Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue; 
+        var res = new ArrayList<List<Integer>>();
+        if (nums[0] > 0) return res; 
+        for (int i = 0; i < n - 2; ++i) {
             if (nums[i] > 0) break; 
-
-            int left = i + 1, right = nums.length - 1;
-            int target = -nums[i];
-
-            while (left < right) {
-                int sum = nums[left] + nums[right];
-                if (sum == target) {
-                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    left++;
-                    right--;
-
-                    
-                    while (left < right && nums[left] == nums[left - 1]) left++;
-                    while (left < right && nums[right] == nums[right + 1]) right--;
-                } else if (sum < target) {
-                    left++;
-                } else {
-                    right--;
-                }
-            }
+            if (i > 0 && nums[i] == nums[i - 1]) continue; 
+            int target = - nums[i];
+            twoSum(nums, i + 1, target, res);
         }
-
         return res;
+    }
+
+    private void twoSum(int[] nums, int i, int target, List<List<Integer>> res) {
+        int j = nums.length -1;
+        while (i < j) {
+            int val = nums[i] + nums[j];
+            if (val > target) {
+                --j;
+                continue;
+            }
+            if (val < target) {
+                ++i;
+                continue;
+            }
+            res.add(Arrays.asList(-target, nums[i], nums[j]));
+            while (i < j && nums[i] == nums[i + 1]) ++i; 
+            while (i < j && nums[j] == nums[j - 1]) --j; 
+            ++i;
+            --j;
+        }
     }
 }
