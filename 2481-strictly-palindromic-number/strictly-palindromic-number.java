d770b0a05555adc26fc31ef211d5eaaff66fb612
Tanishq Shukla 725 <fayoga3821@gmail.com>
2025-11-08 22:50:06 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (5.08%) - LeetSync

--- 2481-strictly-palindromic-number/strictly-palindromic-number.java
+++ 2481-strictly-palindromic-number/strictly-palindromic-number.java
@@ -1,9 +1,7 @@
 class Solution {
     public boolean isStrictlyPalindromic(int n) {
         
-        if(n==1 || n==0) return true;
-        
-        else return false;
+        return false;
 
     }
 }
