4410870e8fbc81389da29a7bb5cda949ca408e77
Tanishq Shukla 725 <fayoga3821@gmail.com>
2025-11-21 17:43:04 +0530

    Time: 16 ms (99.39%) | Memory: 47 MB (17.63%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.java
@@ -1,23 +1,38 @@
 class Solution {
     public int countPalindromicSubsequence(String s) {
-        int[] R = new int[26];
-        for (char u : s.toCharArray()) {
-            R[u - 'a']++;
-        }
-        int[] L = new int[26];
-        HashSet<Integer> S = new HashSet<>();
+        int n = s.length();
+        int[] charFirst = new int[26];
+        int[] posBit = new int[n];
+        Arrays.fill(charFirst, -1);
 
-        for (int i = 0; i < s.length(); i++) {
-            int t = s.charAt(i) - 'a';
-            R[t]--;
-            for (int j = 0; j < 26; j++) {
-                if (L[j] > 0 && R[j] > 0) {
-                    S.add(26 * t + j);
-                }
+        int bits = 0;
+        for (int i = 0; i < n; i++) {
+            int idx = s.charAt(i) - 'a';
+            posBit[i] = bits |= 1 << idx;
+            if (charFirst[idx] == -1) {
+                charFirst[idx] = i;
+                bits = 0;
             }
-            L[t]++;
         }
 
-        return S.size();
+        int res = 0;
+        for (int i = n - 1; i >= 2; i--) {
+            int idx = s.charAt(i) - 'a';
+            if (charFirst[idx] <= -1 || charFirst[idx] == i) {
+                continue;
+            }
+            res += Integer.bitCount(arrOr(charFirst[idx] + 1, i - 1, posBit));
+            charFirst[idx] = -2;
+        }
+        return res;
     }
+
+    private int arrOr(int start, int end, int[] arr) {
+        int res = 0;
+        for (int i=start; i<=end; i++) {
+            res |= arr[i];
+        }
+        return res;
+    }
+
 }
