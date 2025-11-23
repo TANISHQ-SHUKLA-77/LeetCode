f0e55732d4fc237610d9da4148a8091acc994c28
Tanishq Shukla 725 <fayoga3821@gmail.com>
2025-11-23 22:08:55 +0530

    Time: 3 ms (99.69%) | Memory: 48.4 MB (59.32%) - LeetSync

--- 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.java
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.java
@@ -1,27 +1,50 @@
 class Solution {
     public int maxSumDivThree(int[] nums) {
-        int d0 = 0, d1 = 0, d2 = 0;
-        int p0 = 0, p1 = Integer.MIN_VALUE, p2 = Integer.MIN_VALUE;
-        for (int num : nums) {
-            int r = num % 3;
-            if (r == 0) {
-                d0 = p0 + num;
-                d1 = p1 + num;
-                d2 = p2 + num;
-            } else if (r == 1) {
-                d0 = Math.max(p0, p2 + num);
-                d1 = Math.max(p1, p0 + num);
-                d2 = Math.max(p2, p1 + num);
-            } else {
-                d0 = Math.max(p0, p1 + num);
-                d1 = Math.max(p1, p2 + num);
-                d2 = Math.max(p2, p0 + num);
+        int sum = 0;
+        for (int num : nums) sum += num;
+        if (sum % 3 == 0) return sum;
+        else if (sum % 3 == 1) {
+            TwoS mod2 = new TwoS();
+            int mod1 = Integer.MAX_VALUE;
+            for (int num : nums) {
+                if (num % 3 == 2) mod2.add(num);
+                else if (num % 3 == 1) mod1 = Math.min(mod1, num);
             }
-            p0 = d0;
-            p1 = d1;
-            p2 = d2;
+            return Math.max(0, Math.max(sum - mod2.getSum(), sum - mod1));
+        } 
+        else {
+            // case 2 remove 2 smallest %= 1 or % one smallest %= 2
+            TwoS mod1 = new TwoS();
+            int mod2 = Integer.MAX_VALUE;
+            for (int num : nums) {
+                if (num % 3 == 2) mod2 = Math.min(mod2, num);
+                else if (num % 3 == 1) mod1.add(num);
+            }
+            return Math.max(0, Math.max(sum - mod1.getSum(), sum - mod2));
         }
-
-        return d0;
+    }
+    
+    public static class TwoS {
+        int a;
+        int b;
+        
+        public TwoS() {
+            a = Integer.MAX_VALUE;
+            b = Integer.MAX_VALUE;
+        }
+        
+        public void add(int num) {
+            if (num <= a) {
+                b = a;
+                a = num;
+            } else if (num < b) {
+                b = num;
+            }
+        }
+        
+        public int getSum() {
+            if (a == Integer.MAX_VALUE || b == Integer.MAX_VALUE) return Integer.MAX_VALUE;
+            return a + b;
+        }
     }
 }
